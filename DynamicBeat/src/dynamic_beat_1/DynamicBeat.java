3901442ef94c9274ee3edb34026adf705f5dd540
Doosung Jang <teurowel@gmail.com>
2020-05-02 16:52:00 -0400

    Added note, noteroute, judegementline, some Info

--- DynamicBeat/src/dynamic_beat_1/DynamicBeat.java
+++ DynamicBeat/src/dynamic_beat_1/DynamicBeat.java
@@ -2,8 +2,11 @@ package dynamic_beat_1;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -22,6 +25,10 @@ public class DynamicBeat extends JFrame
 	
 	//Based on Main class source file location, load image and get image instance
 	private Image Background = new ImageIcon(Main.class.getResource("../Assets/Images/TitleBackground.jpg")).getImage();
+	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../Assets/Images/GameInfo.png")).getImage();
+	private Image judgementLineImage = new ImageIcon(Main.class.getResource("../Assets/Images/JudgementLine.png")).getImage();
+	private Image noteRouteImage = new ImageIcon(Main.class.getResource("../Assets/Images/NoteRoute.png")).getImage();
+	private Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../Assets/Images/NoteRouteLine.png")).getImage();
 	private Image selectedImage;
 	
 	//Menu bar
@@ -72,6 +79,7 @@ public class DynamicBeat extends JFrame
 	
 	//is main?
 	private boolean isMainScreen = false;
+	private boolean isGameScreen = false;
 	
 	//Music
 	private Music titleMusic = new Music("TitleMusic.mp3", true);
@@ -83,6 +91,12 @@ public class DynamicBeat extends JFrame
 	
 	//Current selected song's idx
 	private int nowSelected = 0; 
+	
+	//Difficulty
+	private String selectedDifficulty = "Easy";
+	
+	//Scroe
+	private int score = 0;
 	
 	public DynamicBeat()
 	{
@@ -156,6 +170,95 @@ public class DynamicBeat extends JFrame
 		
 
 	}
+
+	
+	
+	//Override of JFrame's paint
+	public void paint(Graphics g)
+	{
+		//Create back buffer
+		if(screenImage == null)
+		{
+			screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+			screenGraphic = screenImage.getGraphics();
+		}
+		
+		screenDraw((Graphics2D)screenGraphic);
+		
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics2D g)
+	{
+		//단순한 background image같은건 drawimage로
+		g.drawImage(Background, 0, 0, null);
+		
+		if(isMainScreen)
+		{
+			g.drawImage(selectedImage, 340, 100, null);
+		}
+		else if(isGameScreen)
+		{
+			//Note Route
+			g.drawImage(noteRouteLineImage, 224, 30, null);
+			g.drawImage(noteRouteImage, 228, 30, null);
+			g.drawImage(noteRouteLineImage, 328, 30, null);
+			g.drawImage(noteRouteImage, 332, 30, null);
+			g.drawImage(noteRouteLineImage, 432, 30, null);
+			g.drawImage(noteRouteImage, 436, 30, null);
+			g.drawImage(noteRouteLineImage, 536, 30, null);
+			g.drawImage(noteRouteImage, 540, 30, null);
+			g.drawImage(noteRouteImage, 640, 30, null);
+			g.drawImage(noteRouteLineImage, 740, 30, null);
+			g.drawImage(noteRouteImage, 744, 30, null);
+			g.drawImage(noteRouteLineImage, 844, 30, null);
+			g.drawImage(noteRouteImage, 848, 30, null);
+			g.drawImage(noteRouteLineImage, 948, 30, null);
+			g.drawImage(noteRouteImage, 952, 30, null);
+			g.drawImage(noteRouteLineImage, 1052, 30, null);
+			
+			//Game Info Image
+			g.drawImage(gameInfoImage, 0, 660, null);
+			
+			//Note Judgement Line Image
+			g.drawImage(judgementLineImage, 0, 580, null);
+			
+			//Draw Informations
+			g.setColor(Color.white);
+			g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+			g.setFont(new Font("Arial", Font.BOLD, 30));
+			
+			//Song's name
+			g.drawString(trackList.get(nowSelected).getGameMusic(), 20, 702);
+			
+			//Difficulty
+			g.drawString(selectedDifficulty, 1190, 702);
+			
+			//KeyBoard
+			g.setColor(Color.DARK_GRAY);
+			g.setFont(new Font("Arial", Font.PLAIN, 26));
+			g.drawString("S", 270, 609);
+			g.drawString("D", 374, 609);
+			g.drawString("F", 478, 609);
+			g.drawString("Space Bar", 580, 609);
+			g.drawString("J", 784, 609);
+			g.drawString("K", 889, 609);
+			g.drawString("L", 993, 609);
+			
+			//Score
+			g.setColor(Color.LIGHT_GRAY);
+			g.setFont(new Font("Elephant", Font.BOLD, 30));
+			g.drawString(Integer.toString(score), 565, 702);
+			
+		}
+		
+		
+		//JFrame에 추가된것들(Add를이용해서) Button, Menubar, etc... 그리기
+		paintComponents(g);
+		
+		this.repaint();
+	}
+	
 	
 	private void CreateButton()
 	{
@@ -458,39 +561,6 @@ public class DynamicBeat extends JFrame
 		
 	}
 	
-	
-	//Override of JFrame's paint
-	public void paint(Graphics g)
-	{
-		//Create back buffer
-		if(screenImage == null)
-		{
-			screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-			screenGraphic = screenImage.getGraphics();
-		}
-		
-		screenDraw(screenGraphic);
-		
-		g.drawImage(screenImage, 0, 0, null);
-	}
-	
-	public void screenDraw(Graphics g)
-	{
-		//단순한 background image같은건 drawimage로
-		g.drawImage(Background, 0, 0, null);
-		
-		if(isMainScreen)
-		{
-			g.drawImage(selectedImage, 340, 100, null);
-		}
-		
-		
-		//JFrame에 추가된것들(Add를이용해서) Button, Menubar, etc... 그리기
-		paintComponents(g);
-		
-		this.repaint();
-	}
-	
 	public void EnterMain()
 	{
 		isMainScreen = true;
@@ -569,6 +639,8 @@ public class DynamicBeat extends JFrame
 		}
 		
 		isMainScreen = false;
+		isGameScreen = true;
+		
 		leftButton.setVisible(false);
 		rightButton.setVisible(false);
 		easyButton.setVisible(false);
@@ -576,11 +648,14 @@ public class DynamicBeat extends JFrame
 		backButton.setVisible(true);
 		
 		Background = new ImageIcon(Main.class.getResource("../Assets/Images/" + trackList.get(nowSelected).getGameImage())).getImage();
+		
+		selectedDifficulty = difficulty;
 	}
 	
 	public void BackMain()
 	{
 		isMainScreen = true;
+		isGameScreen = false;
 		
 		leftButton.setVisible(true);
 		rightButton.setVisible(true);
